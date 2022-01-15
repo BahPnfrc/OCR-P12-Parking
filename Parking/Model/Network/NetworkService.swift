3723dd8fb8a98fdbadf73117717a0553fb5398cf
Pierre-Alexandre BAH <pierrealexandre.bah@genapi.fr>
2022-01-15 14:16:14 +0100

    [UPG] - Improved Alamofire requests with Parameters

--- Parking/Model/Network/NetworkService.swift
+++ Parking/Model/Network/NetworkService.swift
@@ -19,18 +19,28 @@ class NetworkService {
         self.bikeDataSession = bikeDataSession
     }
 
+    private struct MetaParams: Encodable {
+        let id: String
+        let utm_source: String
+        let utm_campaign: String
+        let utm_term: String
+    }
+
     // MARK: - Car
-
-    /* URL :
-     https://data.montpellier3m.fr/dataset/disponibilite-des-places-dans-les-parkings-de-montpellier-mediterranee-metropole
-     https://data.montpellier3m.fr/dataset/disponibilite-des-places-velomagg-en-temps-reel/resource/adb98f8d-c4d2-4012-8abe
-     */
-
-    private let carMetaURL = "https://data.montpellier3m.fr/api/3/action/package_show?id=90e17b94-989f-4d66-83f4-766d4587bec2&utm_source=Site%20internet&utm_campaign=Clic%20sur%20%3A%20https%3A//data.montpellier3m.fr/api/3/action/package_show/90e17b94-989f-4d66-83f4-766d4587bec2&utm_term=https%3A//data.montpellier3m.fr/api/3/action/package_show/90e17b94-989f-4d66-83f4-766d4587bec2"
+    
+    /// https://data.montpellier3m.fr/dataset/disponibilite-des-places-dans-les-parkings-de-montpellier-mediterranee-metropole
+    private let carMetaURL = "https://data.montpellier3m.fr/api/3/action/package_show?"
+    private let carMetaParams = MetaParams(
+        id: "90e17b94-989f-4d66-83f4-766d4587bec2",
+        utm_source: "Site%20internet",
+        utm_campaign: "Clic%20sur%20%3A%20https%3A//data.montpellier3m.fr/api/3/action/package_show/90e17b94-989f-4d66-83f4-766d4587bec2",
+        utm_term: "https%3A//data.montpellier3m.fr/api/3/action/package_show/90e17b94-989f-4d66-83f4-766d4587bec2")
 
     func getCarMetaData(
         completion: @escaping (Result<CarMetaData, ApiError>) -> Void) {
-            AF.request(carMetaURL).response { response in
+            AF.request(carMetaURL,
+                       method: .get,
+                       parameters: carMetaParams).response { response in
                 guard let data = response.data,
                         response.error == nil,
                         response.response?.statusCode == 200 else {
@@ -47,10 +57,18 @@ class NetworkService {
 
     // MARK: - Bike
 
-    private let bikeMetaURL = "https://data.montpellier3m.fr/api/3/action/resource_show?id=adb98f8d-c4d2-4012-8abe-cf02903e2ea0&utm_source=Site%20internet&utm_campaign=Clic%20sur%20%3A%20https%3A//data.montpellier3m.fr/api/3/action/resource_show/adb98f8d-c4d2-4012-8abe-cf02903e2ea0&utm_term=https%3A//data.montpellier3m.fr/api/3/action/resource_show/adb98f8d-c4d2-4012-8abe-cf02903e2ea0"
+    /// https://data.montpellier3m.fr/dataset/disponibilite-des-places-velomagg-en-temps-reel/resource/adb98f8d-c4d2-4012-8abe
+    private let bikeMetaURL = "https://data.montpellier3m.fr/api/3/action/resource_show?"
+    private let bikeMetaParams = MetaParams(
+        id: "adb98f8d-c4d2-4012-8abe-cf02903e2ea0",
+        utm_source: "Site%20internet",
+        utm_campaign: "Clic%20sur%20%3A%20https%3A//data.montpellier3m.fr/api/3/action/resource_show/adb98f8d-c4d2-4012-8abe-cf02903e2ea0",
+        utm_term: "https%3A//data.montpellier3m.fr/api/3/action/resource_show/adb98f8d-c4d2-4012-8abe-cf02903e2ea0")
 
     func getBikeMetaData(completion: @escaping (Result<BikeMetadata, ApiError>) -> Void) {
-        AF.request(bikeMetaURL, method: .get).response { response in
+        AF.request(bikeMetaURL,
+                   method: .get,
+                   parameters: bikeMetaParams).response { response in
             guard let data = response.data,
                   response.error == nil,
                   response.response?.statusCode == 200 else {
